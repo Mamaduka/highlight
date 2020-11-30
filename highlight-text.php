3e8515a660c03a6d6495f11ff7068110c8a009a9
George Mamadashvili <georgemamadashvili@gmail.com>
2020-11-30 10:11:23 +0400

    Prepare release

--- highlight-text.php
+++ highlight-text.php
@@ -3,8 +3,8 @@
  * Plugin Name:       Highlight Text
  * Plugin URI:        https://github.com/Mamaduka/highlight
  * Description:       Text highligher for the Block Editor
- * Version:           1.0.2
- * Requires at least: 5.3
+ * Version:           1.1.0
+ * Requires at least: 5.5
  * Requires PHP:      5.6
  * Author:            George Mamadashvili
  * Author URI:        https://mamaduka.com/
